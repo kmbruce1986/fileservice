0c7dd25a7b996f5ea070a0b3ff5295f317ae0321
kmbruce1986 <kmbruce1986@gmail.com>
2018-10-09 16:26:59 +0100

    adds user class

--- src/main/java/com/example/codeclan/fileservice/models/Folder.java
+++ src/main/java/com/example/codeclan/fileservice/models/Folder.java
@@ -12,15 +12,20 @@ public class Folder {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
-    private String name;
+    @Column(name = "title")
+    private String title;
 
     @OneToMany(mappedBy = "folder", fetch = FetchType.LAZY)
     private List<File> files;
 
-    public Folder(String name) {
-        this.name = name;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Folder(String title, User user) {
+        this.title = title;
         this.files = new ArrayList<File>();
+        this.user = user;
     }
 
     public Folder() {
@@ -34,12 +39,12 @@ public class Folder {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getTitle() {
+        return title;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setTitle(String name) {
+        this.title = title;
     }
 
     public List<File> getFiles() {
@@ -48,5 +53,13 @@ public class Folder {
 
     public void setFiles(List<File> files) {
         this.files = files;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
