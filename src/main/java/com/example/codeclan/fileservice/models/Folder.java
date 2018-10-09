f31d8662953435d2b1d0f0a7ff35998cef119280
kmbruce1986 <kmbruce1986@gmail.com>
2018-10-09 16:19:20 +0100

    adds file and folder classes

---
+++ src/main/java/com/example/codeclan/fileservice/models/Folder.java
@@ -0,0 +1,52 @@
+package com.example.codeclan.fileservice.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "folders")
+public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "folder", fetch = FetchType.LAZY)
+    private List<File> files;
+
+    public Folder(String name) {
+        this.name = name;
+        this.files = new ArrayList<File>();
+    }
+
+    public Folder() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+}
